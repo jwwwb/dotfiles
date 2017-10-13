6c8ccacc43c1622b052dba2100a3e27a190220b2
James <james@jwwwb.com>
2017-10-13 22:24:40 +0200

    unhiding dotfiles in repo

--- setup.sh
+++ setup.sh
@@ -20,21 +20,21 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 if [ -e $HOME/.vimrc ]; then
     echo ".vimrc already exists, delete or rename first"
 else
-    ln -s dotfiles/.vimrc $HOME/.vimrc
+    ln -s dotfiles/vimrc $HOME/.vimrc
     echo "symlinked .vimrc"
 fi
 
 if [ -e $HOME/.tmux.conf ]; then
     echo ".tmux.conf already exists, delete or rename first"
 else
-    ln -s dotfiles/.tmux.conf $HOME/.tmux.conf
+    ln -s dotfiles/tmux.conf $HOME/.tmux.conf
     echo "symlinked .tmux.conf"
 fi
 
 if [ -e $HOME/.zshrc ]; then
     echo ".zshrc already exists, delete or rename first"
 else
-    ln -s dotfiles/.zshrc $HOME/.zshrc
+    ln -s dotfiles/zshrc $HOME/.zshrc
     echo "symlinked .zshrc"
 fi
 
