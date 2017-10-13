c5b277d5b0940c3314e0724757af0e18dac851ea
jwwwb <james@jwwwb.com>
2017-10-13 21:32:38 +0200

    adds gnome terminal config for ubuntu

--- setup_linux.sh
+++ setup_linux.sh
@@ -9,3 +9,6 @@ sudo apt-get install zsh
 sudo apt-get install tmux
 sudo apt-get install curl
 
+dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < dotfiles/green_machine.dconf
+
+
